9a693d04ef346fe2cb9fd967cd063313a1b6e96b
Sk2can <generator.03@mail.ru>
2023-10-19 12:01:48 +1000

    Добавлен массив, отслеживающий положение фигур на доске. Созданы методы, обрабатывающие клик по полю.

--- chess_items.py
+++ chess_items.py
@@ -1,35 +1,86 @@
+from math import ceil
+
 import pygame as pg
 from game_config import *
 
 pg.init()
 font_obj = pg.font.Font(pg.font.get_default_font(), 15)
-board = pg.Surface((CELL_SIZE * CELL_QTY, CELL_SIZE * CELL_QTY))
+
 
 class Chessboard:
-    def __init__(self, parent_surface: pg.Surface, cell_qty: int = CELL_QTY, cell_size : int = CELL_SIZE):
+    def __init__(self, parent_surface: pg.Surface):
+        self.board = pg.Surface((CELL_SIZE * CELL_QTY, CELL_SIZE * CELL_QTY))
         self.__screen = parent_surface
-        self.__screen.blit(board, (WINDOW_SIZE[0] - board.get_width(), WINDOW_SIZE[1] - board.get_height()))
-        self.__draw_playboard(cell_qty,cell_size)
+        self.__screen.blit(self.board,
+                           (WINDOW_SIZE[0] - self.board.get_width(), WINDOW_SIZE[1] - self.board.get_height()))
+        self.__draw_playboard()
         pg.display.update()
 
-    def __draw_playboard(self, cell_qty, cell_size):
+    def __draw_playboard(self):
         for y in range(CELL_QTY):
             for x in range(CELL_QTY):
+                # заполняем доску полями
                 cell = pg.Surface((CELL_SIZE, CELL_SIZE))
                 cell.fill(COLORS[(x + y) % 2])
+                # Рисуем координаты полей
                 if y == CELL_QTY - 1:
                     symbol = font_obj.render(ALPHABET[x + 1], 1, COLORS[x % 2])
                     cell.blit(symbol, (5, CELL_SIZE - 15))
                 if x == CELL_QTY - 1:
                     symbol = font_obj.render(str(9 - (y + 1)), 1, COLORS[y % 2])
                     cell.blit(symbol, (CELL_SIZE - 15, 5))
-                board.blit(cell, (x * CELL_SIZE, y * CELL_SIZE))
-        self.__screen.blit(board, (WINDOW_SIZE[0] - board.get_width(), WINDOW_SIZE[1] - board.get_height()))
+                self.board.blit(cell, (x * CELL_SIZE, y * CELL_SIZE))
+        self.__screen.blit(self.board,
+                           (WINDOW_SIZE[0] - self.board.get_width(), WINDOW_SIZE[1] - self.board.get_height()))
+
+    def get_square_from_pos(self, pos):
+        #Получаем координаты ячейки по пикселям
+        y = ceil(pos[0] // 80)
+        x = ceil(pos[1] // 80)
+        return (x, y)
+
+    def get_center_of_cell(self, coords):
+        #вычисляем центр ячейки в пикселях по координатам ячейки
+        x = int(((coords[0]) * CELL_SIZE) + CELL_SIZE/2)
+        y = int(((coords[1]) * CELL_SIZE) + CELL_SIZE/2)
+        return (x, y)
+
+    def handle_click(self, mx, my):
+        x = mx // CELL_SIZE
+        y = my // CELL_SIZE
+        clicked_square = self.get_square_from_pos([x, y])
+        if self.selected_piece is None:
+            if clicked_square.occupying_piece is not None:
+                if clicked_square.occupying_piece.color == self.turn:
+                    self.selected_piece = clicked_square.occupying_piece
+        elif self.selected_piece.move(self, clicked_square):
+            self.turn = 'white' if self.turn == 'black' else 'black'
+        elif clicked_square.occupying_piece is not None:
+            if clicked_square.occupying_piece.color == self.turn:
+                self.selected_piece = clicked_square.occupying_piece
+
 
 class Figure(object):
-    def __init__(self):
-        pass
+    def __init__(self, pos, color, board):
+        self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.color = color
+        self.has_moved = False
 
+    def get_moves(self, board):
+        output = []
+        for direction in self.get_possible_moves(board):
+            for square in direction:
+                if square.occupying_piece is not None:
+                    if square.occupying_piece.color == self.color:
+                        break
+                    else:
+                        output.append(square)
+                        break
+                else:
+                    output.append(square)
+        return output
 
 class Pawn(Figure):
     def __init__(self):
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame as pg
 from game_config import *
 from chess_items import *
 
-
 if __name__ == '__main__':
     clock = pg.time.Clock()
     pg.display.set_caption(WINDOW_TITLE)
@@ -17,3 +16,8 @@ if __name__ == '__main__':
             if event.type == pg.QUIT:
                 pg.quit()
                 run = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    print("Координаты клика: " + str(pg.mouse.get_pos()))
+                    print("Номер клетки: " + str(chessboard.get_square_from_pos(pg.mouse.get_pos())))
+                    print("Координаты центра клетки: " + str(chessboard.get_center_of_cell(chessboard.get_square_from_pos(pg.mouse.get_pos()))) + "\n")
